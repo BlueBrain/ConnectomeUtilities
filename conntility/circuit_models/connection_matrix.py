5d64982d3eed490cd85faa849d8abb9c02fcd196
Michael Reimann <michael@reimann.science>
2021-11-08 12:13:41 +0100

    Added functionality to count synapses between neuron groups

--- conntility/circuit_models/connection_matrix.py
+++ conntility/circuit_models/connection_matrix.py
@@ -1,10 +1,12 @@
 import h5py
 import numpy
 import tqdm
+import pandas
 from scipy import sparse
 
 
 LOCAL_CONNECTOME = "local"
+STR_VOID = "VOID"
 
 
 def find_sonata_connectome(circ, connectome):
@@ -29,14 +31,6 @@ def full_connection_matrix(sonata_fn, n_neurons=None, chunk=50000000):
     return M.tocsr()
 
 
-def circuit_connection_matrix(circ, connectome=LOCAL_CONNECTOME, for_gids=None, chunk=50000000):
-    conn_file = find_sonata_connectome(circ, connectome)
-    N = circ.cells.count()
-    if for_gids is None:
-        return full_connection_matrix(conn_file, n_neurons=N, chunk=chunk)
-    return connection_matrix_for_gids(conn_file, for_gids)
-
-
 def connection_matrix_for_gids(sonata_fn, gids):
     # TODO: Separate gids_pre, gids_post
     idx = numpy.array(gids) - 1  # From gids to sonata "node" indices (base 0 instead of base 1)
@@ -56,3 +50,102 @@ def connection_matrix_for_gids(sonata_fn, gids):
         indptr.append(len(indices))
     mat = sparse.csc_matrix((numpy.ones(len(indices), dtype=bool), indices, indptr), shape=(N, N))
     return mat
+
+
+def circuit_connection_matrix(circ, connectome=LOCAL_CONNECTOME, for_gids=None, chunk=50000000):
+    conn_file = find_sonata_connectome(circ, connectome)
+    N = circ.cells.count()
+    if for_gids is None:
+        return full_connection_matrix(conn_file, n_neurons=N, chunk=chunk)
+    return connection_matrix_for_gids(conn_file, for_gids)
+
+
+def circuit_group_matrices(circ, neuron_groups, connectome=LOCAL_CONNECTOME, extract_full=False, **kwargs):
+    if isinstance(neuron_groups, pandas.DataFrame):
+        neuron_groups = neuron_groups["gid"]
+    if not extract_full:
+        matrices = neuron_groups.apply(lambda grp: circuit_connection_matrix(circ, connectome=connectome,
+                                                                             for_gids=grp.values, **kwargs))
+    else:
+        # TODO: Assumes the full matrix is index from gid 1 to N, which it should. But what if some gids are missing?
+        full_matrix = circuit_connection_matrix(circ, connectome=connectome, **kwargs)
+        matrices = neuron_groups.apply(lambda grp: full_matrix[numpy.ix_(grp.values - 1, grp.values - 1)])
+    return matrices
+
+
+def _make_node_lookup(circ, neuron_groups):
+    from .neuron_groups import flip
+    node_lookup = flip(neuron_groups, contract_values=True, categorical=True)
+    all_gids = circ.cells.ids()
+    missing_gids = numpy.setdiff1d(all_gids, node_lookup.index)
+    node_lookup = pandas.concat([node_lookup,
+                                      pandas.Series([STR_VOID] * len(missing_gids),
+                                                    index=missing_gids)], axis=0)
+    return node_lookup
+
+
+def connection_matrix_between_groups_partition(sonata_fn, node_lookup, chunk=50000000):
+    # TODO: If the user accidently provides a "neuron_groups" instead of "node_lookup" input give helpful message
+    h5 = h5py.File(sonata_fn, "r")['edges/default']
+
+    dset_sz = h5['source_node_id'].shape[0]
+    splits = numpy.arange(0, dset_sz + chunk, chunk)
+
+    midxx = pandas.MultiIndex.from_tuples([], names=["Source node", "Target node"])
+    counts = pandas.Series([], index=midxx, dtype=int)
+
+    for splt_fr, splt_to in tqdm(zip(splits[:-1], splits[1:]), desc="Counting...", total=len(splits) - 1):
+        son_idx_fr = h5['source_node_id'][splt_fr:splt_to]
+        son_idx_to = h5['target_node_id'][splt_fr:splt_to]
+        reg_fr = node_lookup[son_idx_fr + 1]
+        reg_to = node_lookup[son_idx_to + 1]
+        new_counts = pandas.DataFrame({"Source node": reg_fr.values,
+                                       "Target node": reg_to.values}).value_counts()
+        counts = counts.add(new_counts, fill_value=0)
+
+    for lvl, nm in zip(counts.index.levels, counts.index.names):
+        if STR_VOID in lvl:
+            counts = counts.drop(STR_VOID, level=nm)
+    return counts
+
+
+def connection_matrix_between_groups_partial(sonata_fn, node_lookup, chunk=100000):
+    # TODO: If the user accidently provides a "neuron_groups" instead of "node_lookup" input give helpful message
+    h5 = h5py.File(sonata_fn, "r")['edges/default']
+    midxx = pandas.MultiIndex.from_tuples([], names=["Source node", "Target node"])
+    counts = pandas.Series([], index=midxx, dtype=int)
+    buffer_fr = []; buffer_to = []
+
+    use_gids = node_lookup.index.values[node_lookup != STR_VOID]
+
+    for gid in tqdm(use_gids):
+        rnge = h5["indices"]["target_to_source"]["node_id_to_ranges"][gid - 1]
+        for r in h5["indices"]["target_to_source"]["range_to_edge_id"][rnge[0]:rnge[1]]:
+            son_idx_fr = h5["source_node_id"][r[0]:r[1]]
+            son_idx_to = h5["target_node_id"][r[0]:r[1]]
+            reg_fr = node_lookup[son_idx_fr + 1]
+            reg_to = node_lookup[son_idx_to + 1]
+            buffer_fr.append(reg_fr.values); buffer_to.append(reg_to.values)
+        if len(buffer_fr) >= chunk:
+            new_counts = pandas.DataFrame({"Source node": numpy.hstack(buffer_fr),
+                                           "Target node": numpy.hstack(buffer_to)}).value_counts()
+            counts = counts.add(new_counts, fill_value=0)
+            buffer_fr = []; buffer_to = []
+    if len(buffer_fr) > 0:
+        new_counts = pandas.DataFrame({"Source node": numpy.hstack(buffer_fr),
+                                       "Target node": numpy.hstack(buffer_to)}).value_counts()
+        counts = counts.add(new_counts, fill_value=0)
+    for lvl, nm in zip(counts.index.levels, counts.index.names):
+        if STR_VOID in lvl:
+            counts = counts.drop(STR_VOID, level=nm)
+    return counts
+
+
+def circuit_matrix_between_groups(circ, neuron_groups, connectome=LOCAL_CONNECTOME, extract_full=False, **kwargs):
+    node_lookup = _make_node_lookup(circ, neuron_groups)
+    conn_file = find_sonata_connectome(circ, connectome)
+
+    if extract_full:
+        return connection_matrix_between_groups_partition(conn_file, node_lookup, **kwargs)
+    else:
+        return connection_matrix_between_groups_partial(conn_file, node_lookup, **kwargs)
