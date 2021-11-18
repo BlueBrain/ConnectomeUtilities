12f97c20ce0e0d09f37af8b72c45a44015237047
Michael Reimann <michael@reimann.science>
2021-11-18 16:44:05 +0100

    Generalized the lazy matrix in hdf5 format to support dense matrices as well

--- conntility/io/sparse_matrices.py
+++ conntility/io/sparse_matrices.py
@@ -18,6 +18,14 @@ def write_sparse_matrix_payload(hdf_group, dset_pattern="matrix_{0}"):
     return write_dset
 
 
+def write_dense_matrix_payload(hdf_group, dset_pattern="matrix_{0}"):
+    def write_dset(mat):
+        dset_name = dset_pattern.format(len(hdf_group.keys()))
+        hdf_group.create_dataset(dset_name, data=mat)
+        return hdf_group.name + "/" + dset_name
+    return write_dset
+
+
 def read_sparse_matrix_payload(hdf_dset):
     from scipy import sparse
     raw_data = bytes(hdf_dset[:].astype(numpy.uint8))
@@ -26,14 +34,27 @@ def read_sparse_matrix_payload(hdf_dset):
     return mat
 
 
-def write_toc_plus_payload(extracted, to_path, format=None):
+def read_dense_matrix_payload(hdf_dset):
+    return numpy.array(hdf_dset)
+
+
+READ_WRITE = {
+    "sparse": (read_sparse_matrix_payload, write_sparse_matrix_payload),
+    "dense": (read_dense_matrix_payload, write_dense_matrix_payload)
+}
+
+
+def write_toc_plus_payload(extracted, to_path, payload_type="sparse", format=None):
+    # TODO: Could inspect the contents of extraced and determine the payload_type from it.
     path_hdf_store, group_identifier = to_path
     group_identifier_toc = group_identifier + "/toc"
     group_identifier_mat = group_identifier + "/payload"
+    func = READ_WRITE[payload_type][1]
 
     h5_file = h5py.File(path_hdf_store, "a")
     h5_grp_mat = h5_file.require_group(group_identifier_mat)
-    toc = extracted.apply(write_sparse_matrix_payload(h5_grp_mat))
+    h5_grp_mat.attrs["payload_type"] = payload_type
+    toc = extracted.apply(func(h5_grp_mat))
     h5_file.close()
 
     toc.to_hdf(path_hdf_store, key=group_identifier_toc,
@@ -43,27 +64,32 @@ def write_toc_plus_payload(extracted, to_path, format=None):
 class LazyMatrix:
     """..."""
     from lazy import lazy
-    def __init__(self, path_hdf, path_dset):
+
+    def __init__(self, path_hdf, path_dset, func):
         """..."""
         self._hdf = path_hdf
         self._dset = path_dset
+        self._reader = func
 
     @lazy
     def matrix(self):
         """..."""
         with h5py.File(self._hdf, 'r') as hdf:
             dset = hdf[self._dset]
-            matrix = read_sparse_matrix_payload(dset)
+            matrix = self._reader(dset)
         return matrix
 
 
 def read_toc_plus_payload(path):
     path_hdf_store, group_identifier = path
     group_identifier_toc = group_identifier + "/toc"
+    group_identifier_mat = group_identifier + "/payload"
+    with h5py.File(path_hdf_store, "r") as h5:
+        func = READ_WRITE[h5[group_identifier_mat].attrs.get("payload_type", "sparse")]  # Default for bw compatibility
 
     if not os.path.isfile(path_hdf_store):
         raise RuntimeError(f"Missing HDF data at path {path_hdf_store}\n")
 
     toc = pd.read_hdf(path_hdf_store, key=group_identifier_toc)
 
-    return toc.apply(lambda dset_path: LazyMatrix(path_hdf_store, dset_path))
+    return toc.apply(lambda dset_path: LazyMatrix(path_hdf_store, dset_path, func))
